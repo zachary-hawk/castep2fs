8bf024aeb9e9270b3a4194ed9d2cbec39e42c92a
Zachary Hawkhead <zachary.hawkhead@durham.ac.uk>
2023-03-16 09:46:06 +0000

    Update pip

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages
+from setuptools import setup,Extension
+
+setup(name="castep2fs",
+      version="1.0.4",
+      packages=find_packages(),
+      description="CASTEP utility for calculating Fermi surfaces and related properties.",
+      url="https://github.com/zachary-hawk/castep2fs.git",
+      author="Zachary Hawkhead",
+      author_email="zachary.hawkhead@durham.ac.uk",
+      license="MIT",
+      install_requires=["numpy",
+                        "matplotlib",
+                        "scipy",
+                        "ase>=3.18.1",
+                        "pyvista",
+                        "vtk","spglib","argparse","tqdm"],
+
+      entry_points={"console_scripts":["castep2fs=Source.main:main",]
+                    }
+
+      )
+
